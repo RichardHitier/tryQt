840b8eb01cc2f7551758503376b45d71adb2ba79
Richard Hitier <richard.hitier@co-libri.org>
2016-07-09 09:11:20 +0200

    Ping through process

--- ProcessesAndThreads/ProcessesAndThreads/mainwindow.cpp
+++ ProcessesAndThreads/ProcessesAndThreads/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QString>
+#include <QProcess>
+#include <QTimer>
+#include <QDebug>
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,33 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::pingProcess(QString host)
+{
+    QProcess *pingprocess = new QProcess();
+    QString exec = "ping";
+    int status;
+
+    #if defined(win32)
+    QString parameter="-n 1";
+    #else
+    QString parameter="-c 1";
+    #endif
+    //    pingprocess->closereadchannel(qprocess::standardoutput);
+    //    pingprocess->closereadchannel(qprocess::standarderror);
+
+    status = pingprocess->execute(exec, QStringList() << parameter << host);
+    //    pingprocess->setreadchannel(qprocess::standardoutput);
+    //    pingprocess->setreadchannel(qprocess::standarderror);
+    qDebug()<<"pingstatus: "<<status;
+    pingprocess->close();
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    pingProcess("google.com");
+
 }
